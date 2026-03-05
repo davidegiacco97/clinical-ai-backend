00e3084d6b3a304c867489388da6455e08c71003
davidegiacco97 <davide.giacco97@gmail.com>
2026-03-05 18:35:45 +0100

    Update action.ts

--- api/simulation/action.ts
+++ api/simulation/action.ts
@@ -1,1 +1,15 @@
+import { runSimulationTurn } from "@/lib/physiologyEngine"
 
+export default async function handler(req,res){
+
+ if(req.method !== "POST"){
+  return res.status(405).json({error:"Method not allowed"})
+ }
+
+ const { simulationId, action } = req.body
+
+ const result = await runSimulationTurn(simulationId,action)
+
+ return res.json(result)
+
+}
